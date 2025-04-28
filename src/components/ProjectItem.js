67c680c865c00331cf95d1ec9511a48e32411789
+ <moses.mutitu@student.moringaschool.com>
2025-04-28 12:24:06 +0300

    done

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,19 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+  console.log(technologies);
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
         {/* render a <span> for each technology in the technologies array */}
+        <span>Technologies:</span>
+        {technologies.map((technology, index) => (
+          <span key={index} className="technology">
+            {technology}
+          </span>
+        ))}
       </div>
     </div>
   );
